1854e08b21728ee5cbeba9b05d45ae55ba72953d
Teerasej <teerasej@gmaill.com>
2023-12-08 00:40:57 +0700

    create menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ProductCatalog from './pages/ProductCatalog';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div className='flex flex-row shadow bg-black'>
+        <Link to="/" className='p-2 text-white hover:bg-red-700'>Product Catalog</Link>
+        <Link to="/detail" className='p-2  text-white  hover:bg-red-700'>Product Detail</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<ProductCatalog />} />
+        <Route path="/detail" element={<ProductDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
