28b2d698036a138179a275c15c6ffd31c79db4b6
Teerasej <teerasej@gmaill.com>
2023-12-08 00:54:05 +0700

    create simple menu with home button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <nav className=' bg-black flex flex-row'>
+        <p className=' text-white p-4 hover:bg-red-400 active:bg-red-700'>Home</p>
+      </nav>
     </div>
   );
 }
