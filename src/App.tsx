fa8f8ad3cd06a728b93e84237c4724973615d14a
Teerasej <teerasej@gmaill.com>
2023-12-08 09:02:45 +0700

    define a hero image

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,13 @@ function App() {
       <nav className=' bg-black flex flex-row'>
         <p className=' text-white p-4 hover:bg-red-400 active:bg-red-700'>Home</p>
       </nav>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-2xl mt-9'>Welcome to the Product Catalog</h1>
+        <p className='text-xl'>Click on the link above to see the products</p>
+        <div className='sm:w-full sm:mx-3 md:w-full md:mx-3 lg:w-1024 bg-gray-300 rounded flex flex-col items-center mt-10'>
+          <p className='p-32 text-4xl'>Hej!</p>
+        </div>
+      </div>
     </div>
   );
 }
