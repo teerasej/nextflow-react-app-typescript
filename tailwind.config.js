68a0933d4e3c840f171826ebef93ab5c399c6d00
Teerasej <teerasej@gmaill.com>
2023-12-08 08:59:05 +0700

    กำหนดชื่อ style theme เพิ่ม เพื่อใช้ในการ class ของ tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '1024': '1024px',
+      }
+    }
   },
   plugins: [],
 }
